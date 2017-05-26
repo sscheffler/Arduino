7a4beb458c209054cea41128afb8c9e3d432237d
Stefan Scheffler <stefanscheffler82@gmail.com>
2017-05-26 19:30:30 +0200

    improve just_colors

--- led_stripe_fade_in_just_colors/led_stripe_fade_in_just_colors.ino
+++ led_stripe_fade_in_just_colors/led_stripe_fade_in_just_colors.ino
@@ -1,11 +1,11 @@
 
-const int ledRed =10;
+const int ledRed =9;
 const int ledGreen =11;
-const int ledBlue =9;
+const int ledBlue =10;
 const int ownDelay =1000;
 
 
-
+const int d = 2000;
 
 void setup() {
   pinMode(ledRed,OUTPUT);
@@ -15,21 +15,24 @@ void setup() {
 
 
 void loop() {
-  analogWrite(ledRed, 255);  
-  delay(50);
-  analogWrite(ledRed, 0);
-  analogWrite(ledBlue, 255);  
-  delay(50);
-  analogWrite(ledBlue, 0);
-  analogWrite(ledGreen, 255);  
-  delay(50);
-  analogWrite(ledGreen, 0);
-  analogWrite(ledRed, 255);
-  analogWrite(ledBlue, 255);  
-  delay(50);
-  analogWrite(ledRed, 0);
-  analogWrite(ledBlue, 0);  
-  
+  setColor(200,0,0, d);
+  setColor(0,50,0, d);
+  setColor(0,0, 100, d);
+  setColor(255,0,255, d);
+  setColor(255, 20, 0, d);
+  setColor(255, 15, 150, d);
+  resetColor();
+}
+
+void setColor(int r, int g, int b, int d){
+  analogWrite(ledRed, r);
+  analogWrite(ledGreen, g);
+  analogWrite(ledBlue, b); 
+  delay(d) ;
+}
+
+void resetColor(){
+  setColor(0,0,0, d);
 }
 
 
