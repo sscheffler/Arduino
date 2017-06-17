57ab409ce444e60828f1c10182b8bcf67be60485
Stefan Scheffler <stefanscheffler82@gmail.com>
2017-06-17 19:40:49 +0200

    stuff

--- led_simple/led_simple.ino
+++ led_simple/led_simple.ino
@@ -11,7 +11,7 @@
 #define PIN            6
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS     20
+#define NUMPIXELS     16
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
@@ -34,9 +34,9 @@ void loop() {
   for(int i=0;i<NUMPIXELS;i++){
      // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
      //255,172,68
-     pixels.setPixelColor(i, pixels.Color(255,172,68)); // Moderately bright green color.
+     pixels.setPixelColor(i, pixels.Color(100,100,255)); // Moderately bright green color.
      pixels.show(); // This sends the updated pixel color to the hardware.
-  
+     //delay(100);
   }
         
 }
