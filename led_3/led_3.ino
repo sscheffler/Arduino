eac3692c24be219ace2cee82b97098f121bcf726
Stefan Scheffler <stefanscheffler82@gmail.com>
2017-06-18 10:52:53 +0200

    add light sensor detection

--- led_3/led_3.ino
+++ led_3/led_3.ino
@@ -11,15 +11,19 @@
 // Which pin on the Arduino is connected to the NeoPixels?
 // On a Trinket or Gemma we suggest changing this to 1
 #define PIN            6
+#define LIGHT_PIN      A0
+#define PIN_STATUS     7
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      48
+#define NUMPIXELS_STATUS      1
 //48
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
 // example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel statusPixels = Adafruit_NeoPixel(NUMPIXELS_STATUS, PIN_STATUS, NEO_GRB + NEO_KHZ800);
 
 #define delayLed 0
 #define timeOffset 10000
@@ -42,8 +46,37 @@ void setup() {
 }
 
 void loop() {
+  int lightSensor = analogRead(LIGHT_PIN);
   int movingSensor = digitalRead(pirPin);
   int movingSensor_2 = digitalRead(pirPin_2);
+  if(lightSensor < 20){
+    statusPixels.setPixelColor(0, pixels.Color(0,100,0));
+    statusPixels.show();
+    
+    if(delayExceeded()){
+      statusPixels.setPixelColor(0, pixels.Color(0,0,100));
+      statusPixels.show();
+      if ((movingSensor == HIGH || movingSensor_2 == HIGH) && !isUp) {
+        Serial.print(movingSensor);
+        Serial.print("-");
+        Serial.println(movingSensor_2);
+        startTime=millis();
+        on();
+        isUp=true;
+      } else if(isUp) {
+        statusPixels.setPixelColor(0, pixels.Color(0,100,0));
+        statusPixels.show();
+        
+        off();
+        isUp=false;
+      }
+    } else if(movingSensor == HIGH || movingSensor_2 == HIGH) {
+      startTime=millis();
+    }    
+  } else {
+    statusPixels.setPixelColor(0, pixels.Color(100,0,0));
+    statusPixels.show();
+  }
 
 /*if(movingSensor==HIGH && !isUp){
   Serial.print(movingSensor);
@@ -53,22 +86,7 @@ void loop() {
   
   
   //Serial.println(movingSensor);
-  if(delayExceeded()){
-   if ((movingSensor == HIGH || movingSensor_2 == HIGH) && !isUp) {
-    Serial.print(movingSensor);
-    Serial.print("-");
-    Serial.println(movingSensor_2);
-      startTime=millis();
-      on();
-      isUp=true;
-    } else if(isUp) {
-      digitalWrite(13, LOW);
-      off();
-      isUp=false;
-    }
-  } else if(movingSensor == HIGH || movingSensor_2 == HIGH) {
-    startTime=millis();
-  }
+  
 }
 
 boolean delayExceeded(){
